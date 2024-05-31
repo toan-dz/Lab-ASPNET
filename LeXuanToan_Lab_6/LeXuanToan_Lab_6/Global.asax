9293e155693c4b97c819ab99c79830f712a272ae
toan-dz <ltoan5861@gmail.com>
2024-05-31 10:53:30 +0700

    LeXuanToan_Lab_6

---
+++ LeXuanToan_Lab_6/LeXuanToan_Lab_6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeXuanToan_Lab_6.MvcApplication" Language="C#" %>
---
+++ LeXuanToan_Lab_6/LeXuanToan_Lab_6/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeXuanToan_Lab_6.Models
+{
+    public class Member
+    {
+       public int? Id { get; set; }
+
+       public string Username { get; set; }
+
+       public string FullName { get; set; }
+
+       public string Password { get; set; }
+
+       public int? Age { get; set; }
+
+       public string Email { get; set; }
+    }
+}
